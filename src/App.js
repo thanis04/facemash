08e421f562534a9fd129576f5afe25c9d96b0fd5
thanis04 <mtmorion75@gmail.com>
2025-01-23 15:46:18 +0400

    basic level code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import './App.css';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
 import * as faceLandmarksDetection from '@tensorflow-models/face-landmarks-detection'
+import '@tensorflow/tfjs-backend-webgl'
 
 function App() {
 
   const canvasRef = useRef(null)
   const webcamRef = useRef(null)
+
+  const runFacemash = async () => {
+
+    await tf.setBackend("webgl");
+    await tf.ready();
+
+    const model = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh
+    const detectorConfig = {
+      runtime: 'mediapipe',
+      solutionPath: 'https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh',
+      enableSimd: true
+    }
+    const net = await faceLandmarksDetection.createDetector(model, detectorConfig)
+    
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  }
+
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const faces = await net.estimateFaces(video)
+      console.log(faces)
+
+      const ctx = canvasRef.current.getContext("2d");
+      ctx.clearRect(0, 0, videoWidth, videoHeight);
+      ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+
+      if (faces.length > 0) {
+        faces.forEach((face) => {
+          const keypoints = face.keypoints;
+
+          keypoints.forEach((keypoint) => {
+            const [x, y] = [keypoint.x, keypoint.y];
+            ctx.beginPath();
+            ctx.arc(x, y, 2, 0, 2 * Math.PI);
+            ctx.fillStyle = 'aqua'
+            ctx.fill();
+          });
+        });
+      }
+    }
+  }
+
+  useEffect(() => {
+    runFacemash()
+  })
 
   return (
     <div className="App">
